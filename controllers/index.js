323b0f24ff88d805012a74c231e041b424ea4a26
James Brown <cdmdb@hotmail.com>
2020-04-10 14:05:48 +0200

    Added stream error handling. Tuned frecuency of rpc-node requests and depth

--- controllers/index.js
+++ controllers/index.js
@@ -4,7 +4,7 @@ const stringify = require('csv-stringify')
 var CombinedStream = require('combined-stream')
 const fs = require('fs')
 const utils = require('../utils.js')
-const rpcnodes = ['https://anyx.io', 'https://api.hivekings.com', 'https://hived.privex.io', 'https://api.pharesim.me']
+const rpcnodes = ['https://anyx.io', 'https://hived.privex.io', 'https://api.pharesim.me']
 // The readable.pipe() method attaches a Writable stream to the readable, 
 // causing it to switch automatically into flowing mode and push all of its data to the attached Writable. 
 // The flow of data will be automatically managed so that the destination Writable stream is not 
@@ -61,7 +61,7 @@ async function downloadCsv (req, res, next) {
 	console.log(operation, from, until, account)
 	from = new Date(from)
 	until = new Date(until)
-	const depth = 2000
+	const depth = 10000
 
 	var OpCount = await utils.getOpCount(account)
 
@@ -69,7 +69,7 @@ async function downloadCsv (req, res, next) {
 	var untilLimit = false
 	var i = 0
 	var identifier = account + from.getTime().toString() + until.getTime().toString() + operation
-
+	const date1 = new Date()
 	while (fromLimit == false) {
 		let rpcnode = rpcnodes[i % rpcnodes.length]
 		let writeStream = fs.createWriteStream(`./${identifier}${i}.csv`)
@@ -85,6 +85,19 @@ async function downloadCsv (req, res, next) {
 		const data = { "jsonrpc":"2.0", "method":"condenser_api.get_account_history", "params":[account, start, depth], "id":1 }
 		// request response is always a readable type of stream on a client
 		request.post(rpcnode, { form: JSON.stringify(data) }) // .pipe(process.stdout)
+		.on('data', (chunk) => {
+			let data = chunk.toString()
+			let json = {}
+			try {
+				json = JSON.parse(data)
+			} catch(e) {
+				// not an error msg
+			}
+			if (json.hasOwnProperty('error')) {
+				console.log(rpcnode)
+				throw new Error(json.error.message)
+			}
+		})
 		.pipe(JSONStream.parse('result.*', function (item) {
 			let op = item[1].op[1]
 			let timestamp = item[1].timestamp
@@ -105,25 +118,26 @@ async function downloadCsv (req, res, next) {
 		.pipe(stringify(getOptions(operation, i)))
 		.pipe(writeStream)
 
-		await wait(500)
+		await wait(1000)
 		i++
 	}
 	console.log('BINGO ' + i)
-
+	const date2 = new Date()
+	const milisec = 1000
+	const timediff = (date2 - date1) / milisec
+	console.log(timediff)
 	const dir = './'
-	var fileCount = fs.readdirSync(dir).filter((file) => file.indexOf('csv') > -1).length
+	var fileCount = fs.readdirSync(dir).filter((file) => file.indexOf(identifier) > -1).length
 
 	var combinedStream = CombinedStream.create()
 	for (let j = 0; j < fileCount; j++) {
 		let path = `${identifier}${j}.csv`
 		combinedStream.append(fs.createReadStream(path))
-		await wait(500)
-		try {
-		  fs.unlinkSync(path)
-		  //file removed
-		} catch(err) {
-		  console.error(err)
-		}
+		// await wait(500)
+		fs.unlink(path, (err) => {
+		  if (err) throw err;
+		  console.log(path + ' was deleted')
+		})
 	}
 	combinedStream.pipe(res)
 }
